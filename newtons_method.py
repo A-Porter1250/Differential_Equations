378e0af1555e63b385a3506d88765917629b6de3
A-Porter1250 <154635289+A-Porter1250@users.noreply.github.com>
2024-01-31 15:45:28 -0500

    update

--- newtons_method.py
+++ newtons_method.py
@@ -20,3 +20,28 @@ def newton_method_answer(function, derivative, tolerance, max_iterations):
             return round(x_new, 4)
         x = x_new
     return x
+
+def bissection_method(tolerance, max_iterations, range, func):
+    #check convergence condition
+    x = (range[0] + range[1]) / 2
+    if abs(((func(x+0.0001)-func(x-0.0001))/(0.0002))) > 1:
+        print('local derivative causes divergence')
+        return False
+
+
+    #range in form [upper bound, lower bound]
+    error = np.inf
+    iterations = 0
+    while(iterations < max_iterations):
+        x = (range[0] + range[1]) / 2
+        print(x)
+        val = func(x)
+        if abs(val) < tolerance:
+            return x
+        elif val > 0:
+            range[0] = x
+        else:
+            range[1] = x
+        iterations += 1
+    print('unable to converge in max iterations')
+    return False
